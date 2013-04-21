26108562e5cdaeef3b79761afbffce2cd09de975
Aztheroth <bontemps@intechinfo.fr>
2013-04-21 16:28:29 +0200

    j'ai besoin de changer de pc

---
+++ RideOnMotion/RideOnMotion/KinectModule/Tracker/IPositionTracker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RideOnMotion.KinectModule.Tracker
+{
+	interface IPositionTracker
+	{
+		public int positionX { get; set; }
+		public int positionY { get; set; }
+		public int positionZ { get; set; }
+	}
+}
