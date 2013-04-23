00556b6a340deb636eb7af5d24d33421953dce5a
corozyf <bourreaujules@gmail.com>
2013-04-23 10:18:38 +0200

    A first poor version for the log system...

--- RideOnMotion/RideOnMotion/Logger.cs
+++ RideOnMotion/RideOnMotion/Logger.cs
@@ -7,35 +7,50 @@ using System.Threading.Tasks;
 
 namespace RideOnMotion
 {
-	static class Logger
-	{
 
-		/// <summary>
-		/// Write a log in a file
-		/// </summary>
-		/// <param name="toLog"></param>
-		public static void Log(String toLog)
-		{
+    /// <summary>
+    /// First version of the logger on 23.04.2013
+    /// Wait for the new version, by now, I don't have the time to make something cool..
+    /// </summary>
+    static class Logger
+    {
 
-			//open the stream file
-			StreamWriter log;
+        public static void writeLog( String toLog )
+        {
 
-			if (!File.Exists("RideOnMotion.log"))
-			{
-				log = new StreamWriter( "RideOnMotion.log" );
-			}
-			else
-			{
-				log = File.AppendText( "RideOnMotion.log" );
-			}
+            StreamWriter log;
 
-			// Write the détail in the file:
-			log.WriteLine(DateTime.Now);
-			log.WriteLine(toLog);
-			log.WriteLine();
+            if( !File.Exists( "RideOnMotion.log" ) )
+            {
+                log = new StreamWriter( "RideOnMotion.log" );
+            }
+            else
+            {
+                log = File.AppendText( "RideOnMotion.log" );
+            }
 
-			// Close the stream:
-			log.Close();
-		}
-	}
+            log.WriteLine( DateTime.Now );
+            log.WriteLine( toLog );
+            log.WriteLine();
+
+            log.Close();
+        }
+
+        public static String readLog()
+        {
+            StreamReader logReader;
+
+            if( !File.Exists( "RideOnMotion.log" ) )
+            {
+                logReader = new StreamReader( "RideOnMotion.log" );
+                String something = logReader.ReadToEnd();
+                return something;
+            }
+            else
+            {
+                return String.Empty;
+            }
+
+        }
+    }
 }
