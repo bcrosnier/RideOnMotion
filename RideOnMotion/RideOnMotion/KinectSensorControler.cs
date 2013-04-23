7fae2ccb71286ceecac3cf65bc7d86ff56c88396
Benjamin Crosnier <bcrosnier@intechinfo.fr>
2013-04-23 09:53:24 +0200

    KinectSensorController: Added StopSensor, SensorIsRunning, HasSensor.

--- RideOnMotion/RideOnMotion/KinectSensorControler.cs
+++ RideOnMotion/RideOnMotion/KinectSensorControler.cs
@@ -12,29 +12,69 @@ namespace RideOnMotion.KinectModule
 	// gerer le renvoi de status du sensor
 	public class KinectSensorController
 	{
-		KinectSensor _kinectSensor;
+        KinectSensor _kinectSensor = null;
+
+        /// <summary>
+        /// Indicates whether the sensor has been started. Returns false when no sensor was detected.
+        /// </summary>
+        public bool SensorIsRunning
+        {
+            // If _kinectSensor exists, return IsRunning, else return false
+            get { return _kinectSensor != null ? _kinectSensor.IsRunning : false; }
+        }
+
+        /// <summary>
+        /// Indicates whether the Kinect sensor has been detected.
+        /// </summary>
+        public bool HasSensor
+        {
+            // If _kinectSensor exists, return true, else return false
+            get { return _kinectSensor != null ? true : false; }
+        }
 
 		public KinectSensorController()
-		{
-			_kinectSensor = KinectSensor.KinectSensors.Where( item => item.Status == KinectStatus.Connected ).FirstOrDefault();
+        {
+            int deviceCount = KinectSensor.KinectSensors.Count; // Blocking call.
 
-			if( _kinectSensor.SkeletonStream.IsEnabled )
-			{
-				_kinectSensor.SkeletonStream.Enable();
-				//connect event
-				_kinectSensor.SkeletonFrameReady += sensor_SkeletonFrameReady;
-			}
-
-            if ( !_kinectSensor.DepthStream.IsEnabled )
+            if ( deviceCount > 0 )
             {
-                _kinectSensor.DepthStream.Enable();
+                _kinectSensor = KinectSensor.KinectSensors.Where( item => item.Status == KinectStatus.Connected ).FirstOrDefault();
+
+                if ( _kinectSensor.SkeletonStream.IsEnabled )
+                {
+                    _kinectSensor.SkeletonStream.Enable();
+                    //connect event
+                    _kinectSensor.SkeletonFrameReady += sensor_SkeletonFrameReady;
+                }
+
+                if ( !_kinectSensor.DepthStream.IsEnabled )
+                {
+                    _kinectSensor.DepthStream.Enable();
+                }
             }
 		}
 
-		public void StartSensor()
-		{
-			_kinectSensor.Start();
-		}
+        /// <summary>
+        /// Attempts to start the detected Kinect sensor.
+        /// </summary>
+        public void StartSensor()
+        {
+            if ( !SensorIsRunning && HasSensor )
+            {
+                _kinectSensor.Start(); // Blocking call.
+            }
+        }
+
+        /// <summary>
+        /// Attempts to stop the sensor.
+        /// </summary>
+        public void StopSensor()
+        {
+            if ( SensorIsRunning )
+            {
+                _kinectSensor.Stop();
+            }
+        }
 
 		private void sensor_SkeletonFrameReady( object sender, SkeletonFrameReadyEventArgs e )
 		{
