f90362d0068703597a9278573d936e1f98b4bc1d
corozyf <bourreaujules@gmail.com>
2013-04-23 12:08:02 +0200

    The logger no longer write in a file

--- RideOnMotion/RideOnMotion/Logger.cs
+++ RideOnMotion/RideOnMotion/Logger.cs
@@ -12,22 +12,17 @@ namespace RideOnMotion
     /// First version of the logger on 23.04.2013
     /// Wait for the new version, by now, I don't have the time to make something cool..
     /// </summary>
-    static class Logger
+    class Logger
     {
 
-        public static void writeLog( String toLog )
+        MemoryStream memoryStick = new MemoryStream();
+
+        public void writeLog( String toLog )
         {
 
             StreamWriter log;
 
-            if( !File.Exists( "RideOnMotion.log" ) )
-            {
-                log = new StreamWriter( "RideOnMotion.log" );
-            }
-            else
-            {
-                log = File.AppendText( "RideOnMotion.log" );
-            }
+            log = new StreamWriter( this.memoryStick );
 
             log.WriteLine( DateTime.Now );
             log.WriteLine( toLog );
@@ -36,21 +31,13 @@ namespace RideOnMotion
             log.Close();
         }
 
-        public static String readLog()
+        public String readLog()
         {
             StreamReader logReader;
 
-            if( !File.Exists( "RideOnMotion.log" ) )
-            {
-                logReader = new StreamReader( "RideOnMotion.log" );
-                String something = logReader.ReadToEnd();
-                return something;
-            }
-            else
-            {
-                return String.Empty;
-            }
-
+            logReader = new StreamReader( this.memoryStick );
+            String something = logReader.ReadToEnd();
+            return something;
         }
     }
 }
