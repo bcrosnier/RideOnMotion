fc0e4a0899c295e86ffaafae2c04b6b68c997985
Valentin <valbeuz@hotmail.com>
2013-04-24 15:07:55 +0200

    class CaptionArea

---
+++ RideOnMotion/RideOnMotion/KinectModule/ImplementationTracker/CaptionArea.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RideOnMotion.KinectModule.ImplementationTracker
+{
+    class CaptionArea : ICaptionArea
+    {
+        IList<Action> _associateFunctions;
+        IList<Point> _points;
+        Point _topLeftPoint;
+        float _length;
+        float _width;
+        
+		public IReadOnlyList<Action> AssociateFunctions
+		{
+			get { return _associateFunctions.AsReadOnlyList(); }
+		}
+
+        public IReadOnlyList<Point> Points
+        {
+            get
+            {
+                _points = new List<Point>();
+                _points.Add(_topLeftPoint);
+                _topLeftPoint.X += _length;
+                _points.Add(_topLeftPoint);
+                _topLeftPoint.Y -= _width;
+                _points.Add(_topLeftPoint);
+                _topLeftPoint.X -= _length;
+                _points.Add(_topLeftPoint);
+                _topLeftPoint.Y += _width;
+                return _points.AsReadOnlyList();
+            }
+        }
+
+		public CaptionArea(Point topLeftPoint, float length, float width)
+		{
+            if (topLeftPoint == null) throw new ArgumentNullException("point cannot be null");
+			_associateFunctions = new List<Action>();
+            _topLeftPoint = topLeftPoint;
+            _length = length;
+            _width = width;
+		}
+
+        public void AddFunction(Action action)
+        {
+            if (action == null) throw new ArgumentNullException("action cannot be null");
+            _associateFunctions.Add(action);
+        }
+
+        public void RemoveFunction(Action action)
+        {
+            if (action == null) throw new ArgumentNullException("action cannot be null");
+            _associateFunctions.Remove(action);
+        }
+
+        public void CheckPosition(Joint joint)
+        {
+            if (joint.Position.X > _points[0].X && joint.Position.X < _points[1].X && joint.Position.Y > _points[2].Y && joint.Position.Y < _points[0].Y)
+            {
+                foreach (Action action in _associateFunctions)
+                {
+                    action();
+                }
+            }
+        }
+    }
+}
