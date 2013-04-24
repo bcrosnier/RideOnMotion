8523c28aa4da79f4991b4a0164a5d2ac0293c3ff
Valentin <valbeuz@hotmail.com>
2013-04-24 16:15:41 +0200

    fix bug

--- RideOnMotion/RideOnMotion/KinectModule/ImplementationTracker/CaptionArea.cs
+++ RideOnMotion/RideOnMotion/KinectModule/ImplementationTracker/CaptionArea.cs
@@ -27,13 +27,12 @@ namespace RideOnMotion.KinectModule
             {
                 _points = new List<Point>();
                 _points.Add(_topLeftPoint);
-                _topLeftPoint.X += _length;
-                _points.Add(_topLeftPoint);
-                _topLeftPoint.Y -= _width;
-                _points.Add(_topLeftPoint);
-                _topLeftPoint.X -= _length;
-                _points.Add(_topLeftPoint);
-                _topLeftPoint.Y += _width;
+                Point topRightPoint = new Point(_topLeftPoint.X + _length, _topLeftPoint.Y);
+                _points.Add(topRightPoint);
+                Point bottomRightPoint = new Point(topRightPoint.X, topRightPoint.Y + _width);
+                _points.Add(bottomRightPoint);
+                Point bottomLeftPoint = new Point(_topLeftPoint.X, bottomRightPoint.Y);
+                _points.Add(bottomLeftPoint);
                 return _points.AsReadOnlyList();
             }
         }
