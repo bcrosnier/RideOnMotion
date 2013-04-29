6b938e0e25d028057a697584d9b0e6b751e20a72
Ludovic Tresson <tresson@intechinfo.fr>
2013-04-30 01:01:51 +0200

    Base logger and it's tests

--- RideOnMotion/RideOnMotion/Logger.cs
+++ RideOnMotion/RideOnMotion/Logger.cs
@@ -15,5 +15,76 @@ namespace RideOnMotion
     /// </summary>
     public class Logger
     {
+		IDefaultActivityLogger _logger;
+
+		public List<CKTrait> Tags = new List<CKTrait>();
+
+		public void StartLogger()
+		{
+			_logger = new DefaultActivityLogger();
+			_logger.Tap.Register( new StringImpl() );
+		}
+
+		public void RegisterTag( String name )
+		{
+			var tag = ActivityLogger.RegisteredTags.FindOrCreate( name );
+			if(Tags.Where(ExistingTag => ExistingTag.AtomicTraits == tag.AtomicTraits).Any() == false)
+			{
+				Tags.Add( tag );
+			}
+		}
+
+		public void NewEntry(LogLevel logLevel, CKTrait tag, String text)
+		{
+			_logger.UnfilteredLog( tag, logLevel, text, DateTime.UtcNow );
+		}
+		public String Output()
+		{
+			return _logger.Tap.RegisteredSinks.OfType<StringImpl>().Single().Writer.ToString();
+		}
     }
+
+
+	public class StringImpl : IActivityLoggerSink
+	{
+		public StringWriter Writer { get; private set; }
+
+		public StringImpl()
+		{
+			Writer = new StringWriter();
+		}
+
+		public void OnEnterLevel( CKTrait trait, LogLevel level, string text, DateTime time )
+		{
+			time = time.ToLocalTime();
+			Writer.WriteLine();
+			Writer.Write( time.ToString( "H:mm:ss" ) + " [" + level.ToString() + " : "+ trait.ToString() +"] " + text );
+		}
+
+		public void OnContinueOnSameLevel( CKTrait trait, LogLevel level, string text, DateTime time )
+		{
+			time = time.ToLocalTime();
+			Writer.WriteLine();
+			Writer.Write( time.ToString( "H:mm:ss" ) + " [" + level.ToString() + " : " + trait.ToString() + "] " + text );
+		}
+
+		public void OnLeaveLevel( LogLevel level )
+		{
+			Writer.Flush();
+		}
+
+		public void OnGroupOpen( IActivityLogGroup g )
+		{
+			Writer.WriteLine();
+			Writer.Write( new String( '+', g.Depth ) );
+			Writer.Write( "{1} ({0})", g.GroupLevel, g.GroupText );
+		}
+
+		public void OnGroupClose( IActivityLogGroup g, ICKReadOnlyList<ActivityLogGroupConclusion> conclusions )
+		{
+			Writer.WriteLine();
+			Writer.Write( new String( '-', g.Depth ) );
+			Writer.Write( String.Join( ", ", conclusions.Select( c => c.Text ) ) );
+		}
+	}
 }
--- RideOnMotion/UnitTest/Logger.test.cs
+++ RideOnMotion/UnitTest/Logger.test.cs
@@ -4,12 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
-using RideOnMotion;
+using CK.Core;
+using System.IO;
 
 namespace RideOnMotion.test
 {
 	[TestFixture]
-    class LoggerTest
-    {
-    }
+	class LoggerTest
+	{
+		[Test]
+		void logtest()
+		{
+			IDefaultActivityLogger Logger = new DefaultActivityLogger();
+			Logger.Tap.Register( new StringImpl() );
+
+			var tag1 = ActivityLogger.RegisteredTags.FindOrCreate( "Product" );
+			var tag2 = ActivityLogger.RegisteredTags.FindOrCreate( "Sql" );
+			var tag3 = ActivityLogger.RegisteredTags.FindOrCreate( "Combined Tag|Sql|Engine V2|Product" );
+
+			Logger.UnfilteredLog( tag1, LogLevel.Fatal, "Fatal Biatch ", DateTime.UtcNow );
+			Logger.UnfilteredLog( tag1, LogLevel.Fatal, "Fatal Biatch ", DateTime.UtcNow );
+			Logger.UnfilteredLog( tag3, LogLevel.Trace, "Trace Biatch ", DateTime.UtcNow );
+
+			Console.WriteLine( Logger.Tap.RegisteredSinks.OfType<StringImpl>().Single().Writer );
+			Logger.UnfilteredLog( tag2, LogLevel.Info, "Info Biatch ", DateTime.UtcNow );
+			Console.WriteLine( Logger.Tap.RegisteredSinks.OfType<StringImpl>().Single().Writer );
+		}
+		[Test]
+		public void LogTest()
+		{
+			Logger l = new Logger();
+			l.StartLogger();
+			l.RegisterTag( "Test" );
+			l.NewEntry( LogLevel.Info, l.Tags[0], "Try again" );
+			Console.WriteLine( l.Output() );
+		}
+	}
 }
