2f23a719aede61a4ea16072643ec37a130d4bb12
corozyf <bourreaujules@gmail.com>
2013-05-15 16:02:43 +0200

    First version for the TriggerInterpreter

--- RideOnMotion/RideOnMotion.Inputs.Kinect/KinectModule/TriggerInterpreter.cs
+++ RideOnMotion/RideOnMotion.Inputs.Kinect/KinectModule/TriggerInterpreter.cs
@@ -22,19 +22,20 @@ namespace RideOnMotion.Utilities
 		/// <summary>
 		/// interpreter of the trigger value
 		/// </summary>
-		/// <param name="arrayOfTriggerValue">Must contains 4 int(value of roll, pitch, yaw and gaz/elevation</param>
-		public void CommandTraitment(int[] arrayOfTriggerValue)
+		/// <param name="arrayOfTriggerValue">Must contains 8 bool value of (pitch[0-1], roll[2-3], gaz[4-5], yaw[6-7]) triggers</param>
+		public void CommandTraitment(bool[] arrayOfTriggerValue)
 		{
-			int roll = arrayOfTriggerValue[0];
-			int pitch = arrayOfTriggerValue[1];
-			int yaw = arrayOfTriggerValue[2];
-			int gaz = arrayOfTriggerValue[3];
+			int roll; // = arrayOfTriggerValue[2-3];
+			int pitch; // = arrayOfTriggerValue[0-1];
+			int yaw; // = arrayOfTriggerValue[6-7];
+			int gaz; // = arrayOfTriggerValue[4-5];
 
-			if( roll == 3 ) { roll = 1; } else if( roll == 4 ) { roll = -1; } else { roll = 0; }
-			if( pitch == 1 ) { pitch = 1; } else if( pitch == 2 ) { pitch = -1; } else { pitch = 0; }
-			if( yaw == 7 ) { yaw = 1; } else if( yaw == 8 ) { yaw = -1; } else { yaw = 0; }
-			if( gaz == 5 ) { gaz = 1; } else if( gaz == 6 ) { gaz = -1; } else { gaz = 0; }
+			if( arrayOfTriggerValue[2] ) { roll = -1; } else if( arrayOfTriggerValue[3] ) { roll = 1; } else { roll = 0; }
+			if( arrayOfTriggerValue[0] ) { pitch = 1; } else if( arrayOfTriggerValue[1] ) { pitch = -1; } else { pitch = 0; }
+			if( arrayOfTriggerValue[6] ) { yaw = -1; } else if( arrayOfTriggerValue[7] ) { yaw = 1; } else { yaw = 0; }
+			if( arrayOfTriggerValue[4] ) { gaz = 1; } else if( arrayOfTriggerValue[5] ) { gaz = -1; } else { gaz = 0; }
 
+	
 			
 			//navigate( roll, pitch, yaw, gaz );
 
