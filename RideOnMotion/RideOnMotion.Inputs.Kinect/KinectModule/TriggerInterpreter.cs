f93519a068fad3577bc9157093a703682dc5530d
corozyf <bourreaujules@gmail.com>
2013-05-15 11:41:08 +0200

    save this to checkout another branch

---
+++ RideOnMotion/RideOnMotion.Inputs.Kinect/KinectModule/TriggerInterpreter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RideOnMotion.Inputs.Kinect;
+
+namespace RideOnMotion.Utilities
+{
+	class TriggerInterpreter
+	{
+		public TriggerInterpreter()
+		{
+			PositionTrackerController PosTracker = new PositionTrackerController();
+			PosTracker.AreaActived += TriggerRecept;
+		}
+		public void TriggerRecept( object sender, AreaActivedEventArgs e )
+		{
+			AreaActivedEventArgs ReceptedEvent = e;
+			
+		}
+	}
+}
