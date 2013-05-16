45eff535ab1028c057fdbf20325eb05cd469dc8d
Benjamin Crosnier <bcrosnier@intechinfo.fr>
2013-05-16 11:39:56 +0200

    KeyboardController: Added NumPad bindings.

--- RideOnMotion/RideOnMotion.Inputs.Keyboard/KeyboardController.cs
+++ RideOnMotion/RideOnMotion.Inputs.Keyboard/KeyboardController.cs
@@ -34,56 +34,96 @@ namespace RideOnMotion.Inputs.Keyboard
         /// </remarks>
         public void ProcessKey( KeyEventArgs e )
         {
-            //RideOnMotion.Logger.Instance.NewEntry( CK.Core.LogLevel.Trace, RideOnMotion.CKTraitTags.User, "Coin : " + e.Key.ToString() );
+            //RideOnMotion.Logger.Instance.NewEntry( CK.Core.LogLevel.Trace, RideOnMotion.CKTraitTags.User, "Key : " + e.Key.ToString() );
 
             if ( this._drone != null )
             {
                 // Drone control.
                 switch( e.Key.ToString() ) {
+                    // O or Enter: Take off
                     case "O":
                     case "Return":
                         this._drone.Takeoff();
+                        e.Handled = true; // Halt further propagation.
                         break;
 
+                    // Landing
                     case "P":
                     case "Space":
                         this._drone.Land();
+                        e.Handled = true;
                         break;
                         
+                    // LED testing
                     case "L":
                         this._drone.PlayLED();
+                        e.Handled = true;
                         break;
-                        
+                       
+                    // Calibrate flat trim
                     case "F":
                         this._drone.FlatTrim();
+                        e.Handled = true;
                         break;
-                        
-                    case "A":
+                    
+                    /* Navigation commands:
+                     * Navigate( roll, pitch, yaw, gaz )
+                     * Reminder:
+                     * Roll: Translate left or right
+                     * Pitch Translate ahead or backwards
+                     * Yaw: Turn left or right
+                     * Gaz: Propelling power (ie. elevation)
+                     * */
+
+                    /* Arranged like the number pad:
+                     * A Z E  ↖ ↑ ↗
+                     * Q S D  ← ⇎ →
+                     * W X C  ↙ ↓ ↘
+                     * */
+                    case "NumPad7":
+                    case "A": // Fly ahead and to the left?
                         this._drone.Navigate(-1, 1, 0, 0);
+                        e.Handled = true;
                         break;
-                    case "Z":
+                    case "NumPad8":
+                    case "Z": // Fly ahead?
 				        this._drone.Navigate( 0, 1, 0, 0 );
+                        e.Handled = true;
                         break;
-                    case "E":
+                    case "NumPad9":
+                    case "E": // Fly ahead and to the right?
 				        this._drone.Navigate( 1, 1, 0, 0 );
+                        e.Handled = true;
                         break;
-                    case "Q":
+                    case "NumPad4":
+                    case "Q": // Fly left?
 				        this._drone.Navigate( -1, 0, 0, 0 );
+                        e.Handled = true;
                         break;
-                    case "S":
+                    case "NumPad5":
+                    case "S":// Stop
 				        this._drone.Navigate( 0, 0, 0, 0 );
+                        e.Handled = true;
                         break;
-                    case "D":
+                    case "NumPad6":
+                    case "D": // Fly right?
 				        this._drone.Navigate( 1, 0, 0, 0 );
+                        e.Handled = true;
                         break;
-                    case "W":
+                    case "NumPad1":
+                    case "W": // Fly backwards and to the left?
 				        this._drone.Navigate( -1, -1, 0, 0 );
+                        e.Handled = true;
                         break;
-                    case "X":
+                    case "NumPad2":
+                    case "X": // Fly backwards
 				        this._drone.Navigate( 0, -1, 0, 0 );
+                        e.Handled = true;
                         break;
-                    case "C":
+                    case "NumPad3":
+                    case "C": // Fly backwards and to the right?
 				        this._drone.Navigate( 1, -1, 0, 0 );
+                        e.Handled = true;
                         break;
                     default:
                         // Unknown key.
