de0ecaff93ec4481c5afa387ac4a9ab90a8a7a9f
Ludovic Tresson <tresson@intechinfo.fr>
2013-05-21 17:16:24 +0200

    Added Gamepad input

---
+++ RideOnMotion/RideOnMotion.Inputs.Xbox360Gamepad/Xbox360GamepadController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RideOnMotion.Inputs.Xbox360Gamepad
+{
+	class Xbox360GamepadController
+	{
+		volatile bool _keepRunning;
+		XboxController _selectedController;
+
+		public void start()
+		{
+			_selectedController = XboxController.RetrieveController( 0 );
+			_selectedController.StateChanged += _selectedController_StateChanged;
+			XboxController.StartPolling();
+		}
+
+		private void _selectedController_StateChanged( object sender, XboxControllerStateChangedEventArgs e )
+		{
+			SendCommand();
+		}
+
+		private void SendCommand()
+		{
+			RideOnMotion.Logger.Instance.NewEntry( CKLogLevel.Trace, CKTraitTags.User, "X:" + _selectedController.RightThumbStick.X.ToString() + " Y:" + _selectedController.RightThumbStick.Y.ToString() );
+		}
+		public void Close()
+		{
+			XboxController.StopPolling();
+		}
+	}
+}
---
+++ RideOnMotion/RideOnMotion.Inputs.Xbox360Gamepad/XboxControllerStateChangedEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RideOnMotion.Inputs.Xbox360Gamepad
+{
+    public class XboxControllerStateChangedEventArgs: EventArgs
+    {
+        public XInputState CurrentInputState { get; set; }
+        public XInputState PreviousInputState { get; set; }
+    }
+}
