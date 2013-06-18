6015ef41a5275ed4cf0cc253734a3f9bff5fba73
Benjamin Crosnier <bcrosnier@intechinfo.fr>
2013-06-18 15:30:32 +0200

    Partial code cleanup. Don't use. (...)
    - Removed unused AreaActivated event and unused methods from PositionTrackerController
    - Documented PositionTrackerController
    - Partially documented IPositionTracker

--- RideOnMotion/RideOnMotion.Inputs.Kinect/KinectModule/Tracker/IPositionTracker.cs
+++ RideOnMotion/RideOnMotion.Inputs.Kinect/KinectModule/Tracker/IPositionTracker.cs
@@ -7,8 +7,16 @@ using System.Threading.Tasks;
 
 namespace RideOnMotion.Inputs.Kinect
 {
+    /// <summary>
+    /// Position tracker.
+    /// Tracks if a position (typed as T) triggers one of its ICaptionAreas.
+    /// </summary>
+    /// <typeparam name="T">Format of the position</typeparam>
 	public interface IPositionTracker<T>
 	{
+        /// <summary>
+        /// Areas attached to this tracker.
+        /// </summary>
 		IReadOnlyList<ICaptionArea> CaptionAreas { get; }
 
 		void Hooking( T something );
